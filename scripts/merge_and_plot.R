ec0cfb957afe21e675c00bb292cae91f8394c56b
Brian High <bhigh@live.com>
2026-05-01 15:09:21 -0700

    Sorted bars and cleaned up R code

--- scripts/merge_and_plot.R
+++ scripts/merge_and_plot.R
@@ -14,20 +14,21 @@ mlx_data <- read_csv(here("data", "qwen35-9b-mlx_llm_bench.csv"), show_col_types
 gguf_data <- read_csv(here("data", "qwen35-9b-gguf_llm_bench.csv"), show_col_types = FALSE)
 
 # Add a source column to distinguish the datasets
-lmstudio_data$source <- "lmstudio"
-ollama_data$source <- "ollama"
-mlx_data$source <- "qwen3.5-9b-mlx"
-gguf_data$source <- "qwen3.5-9b-gguf"
+lmstudio_data$source <- "LM Studio"
+ollama_data$source <- "Ollama"
+mlx_data$source <- "MLX"
+gguf_data$source <- "GGUF"
 
 # Merge the datasets by combining them
 merged_data_server <- bind_rows(lmstudio_data, ollama_data)
 merged_data_quant_method <- bind_rows(mlx_data, gguf_data)
 
 # Create bar plot with one bar for each server
-aggregated_data_server <- merged_data_server |>
-  group_by(source) %>% summarise(tokens_per_second = mean(tokens_per_second, na.rm = TRUE)) |>
-  mutate(source = ifelse(grepl('lmstudio', source), 'LM Studio', 'Ollama'))
-p1 <- ggplot(aggregated_data_server, aes(x = source, y = tokens_per_second, fill = source)) +
+aggregated_data_server <- merged_data_server |> group_by(source) |>
+  summarise(tokens_per_second = mean(tokens_per_second, na.rm = TRUE))
+p1 <- ggplot(aggregated_data_server,
+             aes(x = reorder(source, -tokens_per_second),
+                 y = tokens_per_second, fill = source)) +
   geom_bar(stat = "identity") +
   geom_text(aes(label = tokens_per_second), vjust = 1.5, color = "white") +
   labs(
@@ -54,10 +55,11 @@ print("Speed ratio: LM Studio (MLX) vs. Ollama (GGUF)")
 print(mean(lmstudio_data$tokens_per_second)/mean(ollama_data$tokens_per_second))
 
 # Create bar plot with one bar for each quantization method
-aggregated_data_quant_method <- merged_data_quant_method |>
-  group_by(source) %>% summarise(tokens_per_second = mean(tokens_per_second, na.rm = TRUE)) |>
-  mutate(source = ifelse(grepl('mlx', source), 'MLX', 'GGUF'))
-p2 <- ggplot(aggregated_data_quant_method, aes(x = source, y = tokens_per_second, fill = source)) +
+aggregated_data_quant_method <- merged_data_quant_method |> group_by(source) |>
+  summarise(tokens_per_second = mean(tokens_per_second, na.rm = TRUE))
+p2 <- ggplot(aggregated_data_quant_method,
+             aes(x = reorder(source, -tokens_per_second),
+                 y = tokens_per_second, fill = source)) +
   geom_bar(stat = "identity") +
   geom_text(aes(label = tokens_per_second), vjust = 1.5, color = "white") +
   labs(
@@ -82,5 +84,3 @@ ggsave(image_file, plot = p2, width = 5, height = 4, dpi = 300)
 # Print the speed ratio of MLX vs. GGUF with LM Studio
 print("Speed ratio: MLX vs. GGUF (both served by LM Studio)")
 print(mean(mlx_data$tokens_per_second)/mean(gguf_data$tokens_per_second))
-
-
